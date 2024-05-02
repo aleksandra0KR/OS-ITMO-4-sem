2f1b03f8a2b79c5ddbb4a34a6da0d6269739cd3a
Aleksandra0KR <kriuchkova.alexa@yandex.ru>
2024-05-02 16:07:29 +0300

    feat: added first task

---
+++ lab5/mem.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+counter=0
+array=()
+echo "" > report.log
+
+while true
+do
+	array+=(1 2 3 4 5 6 7 8 9 10)
+	((counter++))
+
+	if ((counter % 100000 == 0))
+	then
+		echo "Counter = $counter  Size = ${#array[@]}" >> report.log
+	fi
+done
