ddfbae03f34b124f9f5535710ad6fe51e9a58efe
Aleksandra0KR <kriuchkova.alexa@yandex.ru>
2024-05-09 17:55:49 +0300

    feat: added rest of the tasks

--- lab5/newmem.bash
+++ lab5/newmem.bash
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-counter=0
 array=()
 N=$1
 while true
 do
 	
 	array+=(1 2 3 4 5 6 7 8 9 10)
-	((counter++))
 
-	if [[ ${#array[*]} > N ]]
+	if [[ "${#array[*]}" -gt $N ]]
 	then
 		echo "didn't die" >> ans.log
 	
